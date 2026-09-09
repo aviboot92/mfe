adfbdab1dc6db0c2126ee78596338d9c1831148f
Venkat AVINASH Karra <avkarra@gmail.com>
2026-09-09 20:25:56 +0530

    users app is created

---
+++ apps/users/app/page.tsx
@@ -0,0 +1,13 @@
+export default function UsersPage() {
+  return (
+    <main>
+      <h1>Users</h1>
+
+      <p>Users Micro Frontend</p>
+
+      <button>
+        Create User
+      </button>
+    </main>
+  );
+}
