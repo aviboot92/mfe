d35bf4ad7416e240ba4ac8fb843d98ba00d2d1b7
Venkat AVINASH Karra <avkarra@gmail.com>
2026-09-09 21:03:41 +0530

    Added @module-federation/enhanced packages

---
+++ apps/users/app/Users.tsx
@@ -0,0 +1,13 @@
+export default function Users() {
+  return (
+    <section>
+      <h2>Users Micro Frontend</h2>
+
+      <p>Loaded from the Users application.</p>
+
+      <button>
+        Create User
+      </button>
+    </section>
+  );
+}
