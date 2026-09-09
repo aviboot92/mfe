27500161b1fabd7b751b04659a04d423ab1fea4c
Venkat AVINASH Karra <avkarra@gmail.com>
2026-09-09 22:04:49 +0530

    rsbuild is done

--- apps/users-mfe/rsbuild.config.ts
+++ apps/users-mfe/rsbuild.config.ts
@@ -1,10 +1,31 @@
 import { defineConfig } from '@rsbuild/core';
 import { pluginReact } from '@rsbuild/plugin-react';
+import { pluginModuleFederation } from '@module-federation/rsbuild-plugin';
 
-// Docs: https://rsbuild.rs/config/
 export default defineConfig({
   server: {
     port: 3002,
   },
-  plugins: [pluginReact()],
+
+  plugins: [
+    pluginReact(),
+    pluginModuleFederation({
+      name: 'users_mfe',
+
+      exposes: {
+        './Users': './src/Users.tsx',
+      },
+
+      shared: {
+        react: {
+          singleton: true,
+          eager: true,
+        },
+        'react-dom': {
+          singleton: true,
+          eager: true,
+        },
+      },
+    }),
+  ],
 });
